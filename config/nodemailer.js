b5c3cdf56d2a7e63f1b6509569c50163ab2d1ae5
Gagan Suman <fbdgagansuman@gmail.com>
2025-02-05 13:20:20 +0530

    commit

--- config/nodemailer.js
+++ config/nodemailer.js
@@ -4,7 +4,7 @@ import 'dotenv/config'
 
 const transporter = nodemailer.createTransport({
     host: process.envSMTP_HOST,
-    port: process.envSMTP_PORT || 587,
+    port: process.envSMTP_PORT,
     secure:false,
     auth: {
         user:process.env.SMTP_EMAIL_ADDRESS,
